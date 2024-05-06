5c84bae8c75f3fab68fab5558b313a7c5e4b8f8b
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-07 01:06:44 +0430

    Lastest

--- components/client/BestSellers.tsx
+++ components/client/BestSellers.tsx
@@ -87,10 +87,17 @@ const BestSellers = () => {
         }
     }
 
+    const handleClickMore = () => {
+        console.log('handleClickMore')
+    }
+
     return (
         <div className='select-none mt-20 w-full flex flex-col items-center justify-center gap-12 px-16'>
             <div className='flex flex-row items-center justify-between w-full'>
-                <div className='flex flex-row items-center justify-center gap-2 text-lg transition-all duration-200 cursor-pointer text-brown-200 hover:text-white hover:bg-brown-600 hover:scale-105 active:scale-95 rounded-xl px-4 py-2'>
+                <div
+                    className='flex flex-row items-center justify-center gap-2 text-lg transition-all duration-200 cursor-pointer text-brown-200 hover:text-white hover:bg-brown-600 hover:scale-105 active:scale-95 rounded-xl px-4 py-2'
+                    onClick={handleClickMore}
+                >
                     <FaAngleLeft />
                     <h6>مشاهده همه</h6>
                 </div>
