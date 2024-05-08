6d546bea5d199184125eeea74c2a6c8648603e86
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-09 00:59:30 +0430

    fix ui banner

--- components/client/Banner1.tsx
+++ components/client/Banner1.tsx
@@ -1,33 +1,81 @@
+"use client"
+
 import Image from 'next/image';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 const banner1 = '/images/banner-1.png';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Banner1 = () => {
+    const [banners, setBanners] = useState<{
+        id: number, title: string, description: string, image: string
+    }[]>([])
+
+    useEffect(() => {
+        setBanners([
+            {
+                id: 1,
+                title: 'خرید آنلاین انواع قهوه 1',
+                description: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ',
+                image: banner1
+            }, {
+                id: 2,
+                title: 'خرید آنلاین انواع قهوه 2',
+                description: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد ا پسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد ا پسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاتایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد ا ستفاده قرار گیرد.',
+                image: banner1
+            }
+        ])
+    }, [])
+
     return (
         <div className='select-none w-full flex flex-col gap-8 items-center justify-center'>
-            <div className='relative flex flex-row gap-8 items-center justify-between w-[80%] px-16 py-8 bg-[#54372B] rounded-2xl border border-solid border-brown-500'>
-                <Image
-                    className='h-full w-auto -my-20 -ms-16 cursor-pointer'
-                    src={banner1}
-                    alt="banner 1"
-                    width={500}
-                    height={500}
-                    quality={100}
-                    loading='lazy'
-                />
+            <Swiper
+                className="w-full flex flex-row flex-nowrap items-center justify-center"
+                style={{
+                    padding: '32px 0px !important',
+                }}
+                slidesPerView={1}
+                centeredSlides={true}
+                centeredSlidesBounds={true}
+            >
+                {
+                    banners.map((banner) => {
+                        return (
+                            <SwiperSlide key={banner.id}>
+                                <div className='relative flex flex-row gap-8 items-center justify-between mx-auto w-[80%] min-h-96 px-16 py-8 bg-[#54372B] rounded-2xl border border-solid border-brown-500'>
+                                    <Image
+                                        className='h-full w-auto -my-20 -ms-16 cursor-pointer hover:brightness-125 transition-all duration-150'
+                                        src={banner.image}
+                                        alt="banner 1"
+                                        width={500}
+                                        height={500}
+                                        quality={100}
+                                        loading='lazy'
+                                    />
 
-                <h2 dir='rtl' className='py-8 font-light text-xl leading-8 text-brown-200 text-justify'>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-                </h2>
+                                    <h2 dir='rtl' className='w-full py-8 font-light text-xl leading-8 text-brown-200 line-clamp-5 text-ellipsis text-justify'>
+                                        {banner.description}
+                                    </h2>
 
-                <h3 className='absolute -right-5 -top-5 shadow-title-banner text-5xl fontMorvarid'>
-                    خرید آنلاین انواع قهوه
-                </h3>
-            </div>
+                                    <h3 className='absolute -right-5 -top-5 shadow-title-banner text-5xl fontMorvarid'>
+                                        {banner.title}
+                                    </h3>
+                                </div>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
             <div className='w-full flex flex-row gap-8 items-center justify-center'>
-                <span className='flex items-center justify-center text-center size-8 p-4 bg-brown-800 hover:bg-brown-900 transition-all rounded-full cursor-pointer' />
-                <span className='flex items-center justify-center text-center size-8 p-4 bg-brown-500 hover:bg-brown-900 transition-all rounded-full cursor-pointer' />
-                <span className='flex items-center justify-center text-center size-8 p-4 bg-brown-800 hover:bg-brown-900 transition-all rounded-full cursor-pointer' />
+                {
+                    banners.map((banner) => {
+                        return (
+                            <span key={banner.id} className='flex items-center justify-center text-center size-8 p-4 bg-brown-600 hover:bg-brown-900 transition-all rounded-full cursor-pointer' />
+                        )
+                    })
+                }
             </div>
         </div>
     )
