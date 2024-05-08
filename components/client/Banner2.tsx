1aaf98b90b829cc5494fb60f7496fd9f5fd4c91c
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-08 20:28:37 +0430

    fix ui

--- components/client/Banner2.tsx
+++ components/client/Banner2.tsx
@@ -5,7 +5,7 @@ const banner2 = '/images/banner-2.png';
 const Banner2 = () => {
     return (
         <div className='mt-24 select-none w-full flex flex-col gap-8 items-center justify-center'>
-            <div className='overflow-hidden relative flex flex-row gap-8 items-center justify-between w-[80%] px-16 py-8 bg-[#54372B] rounded-2xl border border-solid border-brown-500'>
+            <div className='relative flex flex-row gap-8 items-center justify-between w-[80%] px-16 py-8 bg-[#54372B] rounded-2xl border border-solid border-brown-500'>
                 <h2 dir='rtl' className='py-8 font-light text-xl leading-8 text-brown-200 text-justify'>
                     لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
                 </h2>
@@ -25,7 +25,7 @@ const Banner2 = () => {
                 </h3>
 
                 <Image
-                    className='absolute bottom-0 left-0 w-full opacity-50'
+                    className='absolute bottom-0 left-0 w-full opacity-50 rounded-2xl'
                     src={'/images/svg-wave-2.svg'}
                     width={50}
                     height={50}
