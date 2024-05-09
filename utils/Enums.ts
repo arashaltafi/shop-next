ed8020bf5cb6d2ad304031dc567f0b0a313d885b
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-09 18:05:12 +0430

    utils

---
+++ utils/Enums.ts
@@ -0,0 +1,10 @@
+enum LinkType {
+    TELEGRAM = 'Telegram',
+    INSTAGRAM = 'Instagram',
+    FACEBOOK = 'Facebook',
+    WEBSITE = 'Website',
+}
+
+export {
+    LinkType
+}
---
+++ utils/GlobalUtils.ts
@@ -0,0 +1,7 @@
+const openLink = (link: string) => {
+    window.open(link, '_blank');
+}
+
+export {
+    openLink
+}
