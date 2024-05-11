b6438149e7f529f7440a672fd5e48e98fc0b0200
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-11 12:43:20 +0430

    responsive Footer

--- components/client/Footer.tsx
+++ components/client/Footer.tsx
@@ -49,9 +49,9 @@ const Footer = () => {
   }
 
   return (
-    <div className='relative select-none w-full flex flex-col items-center justify-between gap-8 px-16 pt-8 pb-4'>
+    <div className='relative select-none w-full flex flex-col items-center justify-between gap-8 px-2 sm:px-6 md:px-8 lg:px-14 xl:px-16 pt-2 sm:pt-4 md:pt-6 lg:pt-8 pb-4'>
       <Image
-        className='absolute inset-0 w-full h-full bg-gradient-to-r from-[10%] to-[90%] from-brown-900 to-transparent border-t border-solid border-brown-400 -z-10'
+        className='absolute inset-0 size-full bg-gradient-to-r from-[10%] to-[90%] from-brown-900 to-transparent border-t border-solid border-brown-400 -z-10'
         src={bgFooter}
         alt='background-footer'
         width={1000}
@@ -197,38 +197,40 @@ const Footer = () => {
         </div>
       </div>
 
-      <li dir='rtl' className='w-full text-brown-100 text-sm font-light leading-8 list-disc text-ellipsis text-justify'>
+      <p dir='rtl' className='w-full text-brown-100 text-xs sm:text-sm font-light leading-6 sm:leading-8 list-disc text-ellipsis text-justify'>
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-      </li>
+      </p>
 
-      <div className='w-full flex flex-row gap-8 items-center justify-between bg-brown-100 px-16 py-8 rounded-xl'>
-        <div className='w-full flex flex-row gap-x-8 items-center justify-between bg-yellow-600 rounded-lg px-8 py-4'>
+      <div className='w-full flex flex-row gap-4 sm:gap-6 md:gap-8 items-center justify-between bg-brown-100 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-14 py-2 sm:py-3 md:py-4 lg:py-6 xl:py-8 rounded-md sm:rounded-lg md:rounded-xl'>
+        <div className='w-full flex flex-col-reverse sm:flex-row gap-y-2 gap-x-4 xl:gap-x-0 items-center justify-between bg-yellow-600 rounded-lg px-2 sm:px-4 md:px-6 lg:px-8 py-2 sm:py-3 md:py-4'>
           <button className='btn btnBrown' onClick={handleClickSubmitMail}>
             ارسال
           </button>
           <div className='w-full flex flex-row gap-x-2 items-center justify-end'>
             <input
               dir='ltr'
-              className='w-3/4 px-4 py-2 rounded-md text-base font-light text-black'
+              className='w-full xl:w-3/4 px-2 sm:px-3 md:px-4 py-1 sm:py-1.5 md:py-2 rounded-sm sm:rounded-md text-sm sm:text-base font-light text-black'
               key={'input-footer'}
               type="email"
               placeholder='Email ...'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <EmailRoundedIcon />
+            <div className='hidden lg:block'>
+              <EmailRoundedIcon />
+            </div>
           </div>
         </div>
 
-        <div dir='rtl' className='w-full flex gap-y-2 flex-col text-brown-900'>
-          <h4 className='text-xl font-bold'>اطلاع رسانی</h4>
-          <h6 className='text-lg font-light'>ایمیل‌تان را وارد کنید تا از تخفیف‌ها باخبر شوید.</h6>
+        <div dir='rtl' className='w-full flex gap-y-2 flex-col text-brown-900 text-center sm:text-start'>
+          <h4 className='text-base sm:text-lg md:text-xl font-bold'>اطلاع رسانی</h4>
+          <h6 className='text-sm sm:text-base md:text-lg font-light'>ایمیل‌تان را وارد کنید تا از تخفیف‌ها باخبر شوید.</h6>
         </div>
       </div>
 
-      <span className='w-screen h-0.5 bg-brown-700 rounded-full' />
+      <span className='w-screen h-px sm:h-0.5 bg-brown-700 rounded-full' />
 
-      <div className='w-full flex flex-row items-center justify-center text-brown-100'>
+      <div className='w-full flex flex-col-reverse gap-4 sm:gap-0 sm:flex-row items-center justify-center text-brown-100'>
         <div className='flex flex-row items-start justify-center gap-x-4 child:transition-all child:cursor-pointer child-hover:text-brown-400 child-hover:scale-105 child-active:scale-95'>
           <Tooltip
             TransitionComponent={Fade}
@@ -267,7 +269,7 @@ const Footer = () => {
             </div>
           </Tooltip>
         </div>
-        <h6 dir='rtl' className='flex-1 text-base font-light'>
+        <h6 dir='rtl' className='flex-1 text-sm sm:text-base font-light'>
           کلیه حقوق این سایت متعلق به آنلاین قهوه می باشد.
         </h6>
       </div>
