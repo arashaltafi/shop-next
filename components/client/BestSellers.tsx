6bc2a96bf4bab4fea40a03af2928735e3a1661ba
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-12 23:27:59 +0430

    add react-parallax-tilt animtion

--- components/client/BestSellers.tsx
+++ components/client/BestSellers.tsx
@@ -4,6 +4,7 @@ import { convertPrice } from '@/utils/Product';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded';
+import Tilt from 'react-parallax-tilt';
 const product1 = '/images/p1.png';
 const product2 = '/images/p2.png';
 const product3 = '/images/p3.png';
@@ -112,48 +113,63 @@ const BestSellers = () => {
             >
                 {
                     products.map(product => (
-                        <div
+                        <Tilt
                             key={product.id}
-                            className='relative bg-brown-900/5 group cursor-pointer flex flex-col items-center justify-center gap-4 rounded-xl p-[1px] border-2 border-solid border-brown-700 text-brown-200 hover:text-brown-100 hover:bg-brown-900 transition-all duration-200'>
-                            <div className='relative w-full'>
-                                <Image
-                                    className='w-full h-44 sm:h-48 md:h-56 lg:h-60 xl:h-64 group-hover:brightness-110 transition-all bg-brown-500 rounded-t-xl rounded-b-sm'
-                                    src={product.image}
-                                    alt={product.name}
-                                    width={200}
-                                    height={200}
-                                    quality={100}
-                                    loading='lazy'
-                                />
-                                <span
-                                    id={`product-${product.id}`}
-                                    className='absolute top-0 right-0 left-0 bottom-0 bg-gradient-to-bl from-[50%] via-[50%] to-[100%] from-transparent via-transparent to-brown-800 rounded-t-xl rounded-b-sm'
-                                />
-                            </div>
+                            gyroscope={true}
+                            glareEnable={true}
+                            glareMaxOpacity={0.45}
+                            tiltMaxAngleX={5}
+                            tiltMaxAngleY={5}
+                            transitionSpeed={3000}
+                            perspective={1000}
+                            tiltEnable={true}
+                            tiltReverse={false}
+                            reset={true}
+                            glareColor={'#221518'}
+                        >
+                            <div
+                                className='relative bg-brown-900/5 group cursor-pointer flex flex-col items-center justify-center gap-4 rounded-xl p-[1px] border-2 border-solid border-brown-700 text-brown-200 hover:text-brown-100 hover:bg-brown-900 transition-all duration-200'>
 
-                            <div className='flex flex-col items-center justify-center gap-2 font-light pb-4 sm:pb-8'>
-                                <h3 id={`product-${product.id}`} dir='rtl' className='font-bold text-sm md:text-base lg:text-lg xl:text-xl text-ellipsis line-clamp-2 max-w-64 text-center'>
-                                    {product.name}
-                                </h3>
-                                <div className='flex flex-col items-center justify-center'>
-                                    <h4 dir='rtl' className='mt-2 fontMorvarid text-xs md:text-sm lg:text-base xl:text-lg text-ellipsis line-clamp-1 max-w-64'>
-                                        {product.description}
-                                    </h4>
-                                    <h5 dir='rtl' className='text-xs md:text-sm lg:text-base xl:text-lg text-ellipsis line-clamp-1 max-w-64'>
-                                        <span className='fontMorvarid'>{convertPrice(50000)}</span> تومان
-                                    </h5>
+                                <div className='relative w-full'>
+                                    <Image
+                                        className='w-full h-44 sm:h-48 md:h-56 lg:h-60 xl:h-64 group-hover:brightness-110 transition-all bg-brown-500 rounded-t-xl rounded-b-sm'
+                                        src={product.image}
+                                        alt={product.name}
+                                        width={200}
+                                        height={200}
+                                        quality={100}
+                                        loading='lazy'
+                                    />
+                                    <span
+                                        id={`product-${product.id}`}
+                                        className='absolute top-0 right-0 left-0 bottom-0 bg-gradient-to-bl from-[50%] via-[50%] to-[100%] from-transparent via-transparent to-brown-800 rounded-t-xl rounded-b-sm'
+                                    />
                                 </div>
-                                <div id={`order-${product.id}`} className='block sm:hidden text-sm px-3 py-1.5 bg-brown-600 text-brown-200 drop-shadow-md rounded-md hover:bg-green-500'>
+
+                                <div className='flex flex-col items-center justify-center gap-2 font-light pb-4 sm:pb-8'>
+                                    <h3 id={`product-${product.id}`} dir='rtl' className='font-bold text-sm md:text-base lg:text-lg xl:text-xl text-ellipsis line-clamp-2 max-w-64 text-center'>
+                                        {product.name}
+                                    </h3>
+                                    <div className='flex flex-col items-center justify-center'>
+                                        <h4 dir='rtl' className='mt-2 fontMorvarid text-xs md:text-sm lg:text-base xl:text-lg text-ellipsis line-clamp-1 max-w-64'>
+                                            {product.description}
+                                        </h4>
+                                        <h5 dir='rtl' className='text-xs md:text-sm lg:text-base xl:text-lg text-ellipsis line-clamp-1 max-w-64'>
+                                            <span className='fontMorvarid'>{convertPrice(50000)}</span> تومان
+                                        </h5>
+                                    </div>
+                                    <div id={`order-${product.id}`} className='block sm:hidden text-sm px-3 py-1.5 bg-brown-600 text-brown-200 drop-shadow-md rounded-md hover:bg-green-500'>
+                                        سفارش
+                                    </div>
+                                </div>
+
+                                <div id={`order-${product.id}`} className='hidden sm:block z-40 text-base absolute -bottom-7 lg:-bottom-8 px-4 lg:px-5 py-2 md:py-2.5 lg:py-3 bg-brown-600 text-brown-200 drop-shadow-md rounded-xl hover:bg-green-500 hover:scale-[101%] active:scale-[99%]'>
                                     سفارش
                                 </div>
-                            </div>
 
-                            <div id={`order-${product.id}`} className='hidden sm:block z-40 text-base absolute -bottom-7 lg:-bottom-8 px-4 lg:px-5 py-2 md:py-2.5 lg:py-3 bg-brown-600 text-brown-200 drop-shadow-md rounded-xl hover:bg-green-500 hover:scale-[101%] active:scale-[99%]'>
-                                سفارش
+                                <span id={`product-${product.id}`} className='hidden sm:block z-30 absolute inset-0' />
                             </div>
-
-                            <span id={`product-${product.id}`} className='hidden sm:block z-30 absolute inset-0' />
-                        </div>
+                        </Tilt>
                     ))
                 }
             </div>
--- components/client/Categories.tsx
+++ components/client/Categories.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
+import Tilt from 'react-parallax-tilt';
 const equipment = '/images/category1.png';
 const espresso = '/images/category2.png';
 const sweets = '/images/category3.png';
@@ -51,15 +52,29 @@ const Categories = () => {
                         <div
                             key={item.id}
                             className='flex flex-col gap-4 items-center justify-center group transition-all cursor-pointer hover:scale-[101%] active:scale-[99%]'>
-                            <Image
-                                className='size-32 sm:size-36 md:size-40 lg:size-44 xl:size-48 group-hover:brightness-110 transition-all overflow-hidden'
-                                src={item.image}
-                                alt={item.title}
-                                width={200}
-                                height={200}
-                                quality={100}
-                                loading='lazy'
-                            />
+                            <Tilt
+                                gyroscope={true}
+                                glareEnable={true}
+                                glareMaxOpacity={0.45}
+                                tiltMaxAngleX={20}
+                                tiltMaxAngleY={20}
+                                transitionSpeed={2500}
+                                perspective={500}
+                                tiltEnable={true}
+                                tiltReverse={false}
+                                reset={true}
+                                glareColor={'#221518'}
+                            >
+                                <Image
+                                    className='size-32 sm:size-36 md:size-40 lg:size-44 xl:size-48 group-hover:brightness-110 transition-all overflow-hidden'
+                                    src={item.image}
+                                    alt={item.title}
+                                    width={200}
+                                    height={200}
+                                    quality={100}
+                                    loading='lazy'
+                                />
+                            </Tilt>
                             <h6 className='text-sm md:text-base lg:text-lg xl:text-xl text-brown-100 group-hover:brightness-110 transition-all font-light text-nowrap text-ellipsis line-clamp-1 text-center'>
                                 {item.title}
                             </h6>
