8f61a6d010aaa79c4053aa12388b29b4d95658c7
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-14 00:12:50 +0430

    Support, Contact

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,8 +1,20 @@
+import Footer from '@/components/client/Footer'
+import Header from '@/components/client/Header'
 import React from 'react'
 
 const Support = () => {
     return (
-        <div>Support</div>
+        <div className='flex flex-col items-center justify-center w-full'>
+            <Header isFromMainHeader={false} />
+            <div className='flex flex-col gap-16 items-center justify-center w-full'>
+                <p> </p>
+                <p> </p>
+                <p> </p>
+                <p> </p>
+                <p> </p>
+            </div>
+            <Footer />
+        </div>
     )
 }
 
