ff55397ce6556bc6ad7a2fcd55609b24368159a4
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-15 00:45:40 +0430

    fix about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -48,7 +48,7 @@ const About = () => {
         <div className='select-none w-full flex flex-col gap-24 sm:gap-36 md:gap-48 items-center justify-start'>
             <Header isFromMainHeader={false} />
             <div className='flex flex-col gap-8 items-center justify-center w-full px-2 sm:px-5 md:px-8 lg:px-14 xl:px-16'>
-                <h1 dir='rtl' className='self-end text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>داستان قهوه آنلاین:</h1>
+                <h2 dir='rtl' className='self-end text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>داستان قهوه آنلاین:</h2>
                 <div className='mt-8 flex flex-col md:flex-row items-center justify-center gap-8'>
                     <Image
                         src={'/images/coffee-cup-1.png'}
@@ -68,7 +68,7 @@ const About = () => {
 
                 <DividerCoffee />
 
-                <h1 dir='rtl' className='self-end text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>اهداف قهوه آنلاین:</h1>
+                <h2 dir='rtl' className='self-end text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>اهداف قهوه آنلاین:</h2>
                 <div className='mt-8 flex flex-col md:flex-row items-center justify-center gap-8'>
                     <p dir='rtl' className='w-full text-brown-100 text-sm font-light leading-6 sm:leading-8 list-disc text-ellipsis text-center md:text-justify'>
                         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
@@ -88,7 +88,7 @@ const About = () => {
 
                 <DividerCoffee />
 
-                <h1 dir='rtl' className='text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>ارزش های قهوه آنلاین:</h1>
+                <h2 dir='rtl' className='text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>ارزش های قهوه آنلاین:</h2>
                 <div className='mt-16 w-full grid justify-items-center grid-cols-1 sm:grid-cols-2 items-center justify-center gap-8 -px-2 sm:-px-5 md:-px-8 lg:-px-14 xl:-px-16'>
                     {
                         items.map((item) => (
@@ -103,7 +103,7 @@ const About = () => {
 
                 <DividerCoffee />
 
-                <h1 dir='rtl' className='self-end text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>چشم انداز قهوه آنلاین:</h1>
+                <h2 dir='rtl' className='self-end text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>چشم انداز قهوه آنلاین:</h2>
                 <div className='mt-8 flex flex-col-reverse md:flex-row items-center justify-center gap-8'>
                     <ul dir='rtl' className='w-full flex flex-col items-center md:items-start justify-center gap-8 text-center'>
                         <li className='text-sm sm:text-base md:text-lg list-disc'>ایجاد برندینگ قوی</li>
