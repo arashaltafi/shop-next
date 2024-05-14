cec4c764a6490d228befc95b5c42cf129a74a6cb
arashaltafi <altafi.a@nahad.ir>
2024-05-14 10:44:43 +0330

    404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,29 +2,46 @@ import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import Header from '@/components/client/Header';
+import CoffeeBeans from '@/components/client/CoffeeBeans';
+import Footer from '@/components/client/Footer';
 
 const NotFound = () => {
     return (
-        <div
-            className='select-none w-full flex flex-col gap-12 items-center justify-around px-10 sm:px-12 md:px-14 lg:px-16 py-4 sm:py-6 md:py-8 overflow-hidden'>
-            <Image
-                className='size-72 sm:size-80 md:size-96 shake2'
-                src='/images/404.png'
-                alt='404-notFound'
-                width={500}
-                height={500}
-                priority
-                loading='eager'
-                quality={100}
-            />
+        <div className='select-none w-full flex flex-col gap-28 items-center justify-start'>
+            <Header isFromMainHeader={false} />
 
-            <h1 dir='rtl' className='text-xl sm:text-2xl md:text-3xl lg:text-4xl'>
-                صفحه  12 مورد نظر یافت نشد !!!
-            </h1>
+            <div className='w-full flex flex-col gap-16 items-center justify-center px-10 sm:px-12 md:px-14 lg:px-16 py-4 sm:py-6 md:py-8'>
+                <div className='flex flex-row items-center justify-center shake2'>
+                    <h2 className='text-7xl sm:text-8xl md:text-9xl text-brown-200'>4</h2>
+                    <Image
+                        className='size-28 sm:size-40 md:size-48 brightness-150'
+                        src='/images/logo.png'
+                        alt='404-notFound'
+                        width={500}
+                        height={500}
+                        priority
+                        loading='eager'
+                        quality={100}
+                    />
+                    <h2 className='text-7xl sm:text-8xl md:text-9xl text-brown-200'>4</h2>
+                </div>
 
-            <Link href={'/'} className='btn btnPrimary'>
-                Back To Home
-            </Link>
+                <h1 dir='rtl' className='text-md sm:text-lg md:text-xl lg:text-2xl'>
+                    صفحه مورد نظر یافت نشد !!!
+                </h1>
+
+                <Link href={'/'} className='btn btnLightBrown flex'>
+                    <h3>
+                        بازگشت به صفحه اصلی
+                    </h3>
+
+                    <KeyboardArrowRightIcon className='ml-3' />
+                </Link>
+            </div>
+
+            <Footer />
         </div>
     )
 }
