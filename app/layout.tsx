bbc054dcd8d3bc3272c439efd44ffccafd40e40a
arashaltafi <altafi.a@nahad.ir>
2024-05-14 13:24:55 +0330

    fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,32 +8,40 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 const shabnamFont = localFont({
   src: [
     {
-      path: '../public/fonts/font_bold.ttf',
+      path: '../public/fonts/iranYekan/IRANYekanX-ExtraBold.ttf',
+      weight: '900'
+    },
+    {
+      path: '../public/fonts/iranYekan/IRANYekanX-ExtraBold.ttf',
       weight: '800'
     },
     {
-      path: '../public/fonts/font_bold.ttf',
+      path: '../public/fonts/iranYekan/IRANYekanX-DemiBold.ttf',
       weight: '700'
     },
     {
-      path: '../public/fonts/font_bold.ttf',
+      path: '../public/fonts/iranYekan/IRANYekanX-Bold.ttf',
       weight: '600'
     },
     {
-      path: '../public/fonts/font_normal.ttf',
+      path: '../public/fonts/iranYekan/IRANYekanX-Medium.ttf',
       weight: '500'
     },
     {
-      path: '../public/fonts/font_normal.ttf',
+      path: '../public/fonts/iranYekan/IRANYekanX-Medium.ttf',
       weight: '400'
     },
     {
-      path: '../public/fonts/font_medium.ttf',
+      path: '../public/fonts/iranYekan/IRANYekanX-Light.ttf',
       weight: '300'
     },
     {
-      path: '../public/fonts/font_light.ttf',
+      path: '../public/fonts/iranYekan/IRANYekanX-Thin.ttf',
       weight: '200'
+    },
+    {
+      path: '../public/fonts/iranYekan/IRANYekanX-UltraLight.ttf',
+      weight: '100'
     }
   ]
 })
