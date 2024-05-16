7a6c269d2a04d07658d390b25736c6860bdfdac8
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-17 01:46:14 +0430

    set Loading

--- components/client/LandingPage.tsx
+++ components/client/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import MainHeader from './MainHeader'
 import Banner1 from './Banner1'
 import CoffeeBeans from './CoffeeBeans'
@@ -14,17 +14,19 @@ import Footer from './Footer'
 const LandingPage = () => {
   return (
     <div className='flex flex-col gap-12 sm:gap-16 md:gap-20 items-center justify-start w-full'>
-      <CoffeeBeans />
-      <MainHeader />
-      <Banner1 />
-      <Categories />
-      <BestSellers />
-      <Banner2 />
-      <Lastest />
-      <Comments />
-      <Menu />
-      <Services />
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CoffeeBeans />
+        <MainHeader />
+        <Banner1 />
+        <Categories />
+        <BestSellers />
+        <Banner2 />
+        <Lastest />
+        <Comments />
+        <Menu />
+        <Services />
+        <Footer />
+      </Suspense>
     </div>
   )
 }
