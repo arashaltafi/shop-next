7793f0120d04cdbfb625ab37bf7cab6e1d0b660b
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-17 00:21:29 +0430

    add api route

---
+++ app/api/route.ts
@@ -0,0 +1,23 @@
+export async function GET() {
+    return Response.json({ message: 'Welcome to Coffee Shop Online Backend!' }, { status: 200 })
+}
+
+export async function POST() {
+    return Response.json({ message: 'Welcome to Coffee Shop Online Backend!' }, { status: 200 })
+}
+
+export async function PUT() {
+    return Response.json({ message: 'Welcome to Coffee Shop Online Backend!' }, { status: 200 })
+}
+
+export async function UPDATE() {
+    return Response.json({ message: 'Welcome to Coffee Shop Online Backend!' }, { status: 200 })
+}
+
+export async function DELETE() {
+    return Response.json({ message: 'Welcome to Coffee Shop Online Backend!' }, { status: 200 })
+}
+
+export async function PATCH() {
+    return Response.json({ message: 'Welcome to Coffee Shop Online Backend!' }, { status: 200 })
+}
