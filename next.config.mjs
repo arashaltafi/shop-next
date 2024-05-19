7caa731522c8c1d146faa3530be49ece1001c3b8
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-20 00:38:03 +0430

    add next pwa for service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,28 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+// way 1 (work success!)
+// https://ducanh-next-pwa.vercel.app/docs/next-pwa/getting-started
+// npm i @ducanh2912/next-pwa 
+// npm i -D webpack
+// npm i next-pwa
+
+// way 2
+// https://github.com/shadowwalker/next-pwa
+// https://www.npmjs.com/package/next-pwa
+// npm i next-pwa
+
+const pwa = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+//   scope: '/app',
+//   sw: 'service-worker.js',
+//   fallbacks: {
+//       document: '/offline',
+//   },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
@@ -9,4 +34,4 @@ const nextConfig = {
     }
 };
 
-export default nextConfig;
+export default pwa(nextConfig);
