7992b245b56a3bbb9556b45c6b679117c7d8edfb
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-22 11:35:34 +0430

    add Categories listener

--- components/client/Categories.tsx
+++ components/client/Categories.tsx
@@ -2,6 +2,7 @@
 
 import { getDeviceSize } from '@/utils/GlobalUtils';
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import Tilt from 'react-parallax-tilt';
 const equipment = '/images/category1.png';
@@ -12,6 +13,8 @@ const typesCoffee = '/images/category5.png';
 const bg4 = '/images/bg-4.png';
 
 const Categories = () => {
+    const router = useRouter()
+
     const [category, setCategories] = useState<{
         id: number, title: string, image: string
     }[]>([])
@@ -42,17 +45,29 @@ const Categories = () => {
         ])
     }, [])
 
+    const handleClickCategory = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const target = e.target as HTMLDivElement
+        if (target.id === 'undefined' || target.id === '' || target.id === null) return
+
+        if (target.id.includes('category')) {
+            const targetId = target.id.replace('category-', '')
+            router.push(`category/${targetId}`)
+        }
+    }
+
     return (
         <div className='relative select-none md:mt-12 lg:mt-16 xl:mt-20 w-full flex flex-col items-center justify-center gap-x-6 md:gap-x-8 lg:gap-x-10 xl:gap-x-12 gap-y-12 px-2 sm:px-5 md:px-8 lg:px-14 xl:px-16'>
             <h2 dir='rtl' className='self-end text-brown-200 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl fontMorvarid'>
                 دسته بندی محصولات:
             </h2>
-            <div className='flex flex-row flex-wrap items-center justify-center gap-6 md:gap-8 lg:gap-10 xl:gap-12'>
+            <div
+                onClick={(e) => handleClickCategory(e)}
+                className='flex flex-row flex-wrap items-center justify-center gap-6 md:gap-8 lg:gap-10 xl:gap-12'>
                 {
                     category.map((item) => (
                         <div
                             key={item.id}
-                            className='flex flex-col gap-4 items-center justify-center group transition-all cursor-pointer hover:scale-[101%] active:scale-[99%]'>
+                            className='relative flex flex-col gap-4 items-center justify-center group transition-all cursor-pointer hover:scale-[101%] active:scale-[99%]'>
                             <Tilt
                                 gyroscope={true}
                                 glareEnable={true}
@@ -79,6 +94,8 @@ const Categories = () => {
                             <h6 className='text-sm md:text-base lg:text-lg xl:text-xl text-brown-100 group-hover:brightness-110 transition-all font-light text-nowrap text-ellipsis line-clamp-1 text-center'>
                                 {item.title}
                             </h6>
+
+                            <span id={`category-${item.title}`} className='absolute top-0 right-0 left-0 bottom-0' />
                         </div>
                     ))
                 }
