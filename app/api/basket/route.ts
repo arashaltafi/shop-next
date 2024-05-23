c451bcb188a093d4f075b8a209bfe2781feec822
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-23 20:10:50 +0430

    fix basket api

--- app/api/basket/route.ts
+++ app/api/basket/route.ts
@@ -6,16 +6,22 @@ export async function POST(req: NextRequest) {
 
     const data = basketList.map((basket: any) => {
         const product = productList.find((product: any) => product.productId === basket.productId)
-        return {
-            id: product?.id,
-            productId: basket.productId,
-            quantity: basket.quantity,
-            name: product?.name,
-            description: product?.description,
-            image: product?.image,
-            price: product?.price
+        if (product) {
+            return {
+                id: product?.id,
+                productId: basket.productId,
+                quantity: basket.quantity,
+                name: product?.name,
+                description: product?.description,
+                image: product?.image,
+                price: product?.price
+            }
         }
     })
+
+    if (data === undefined || data === null || data.length === 0 || data[0] === null) {
+        return Response.json({ error: 'موردی یافت نشد' }, { status: 404 })
+    }
 
     return Response.json({ data: data }, { status: 200 })
 }
