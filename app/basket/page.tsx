c4b4ca0f2b9eccd06ab410d39529bb8012fbb70f
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-24 17:28:24 +0430

    fix ui basket

--- app/basket/page.tsx
+++ app/basket/page.tsx
@@ -15,9 +15,13 @@ import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
 import { addProductToStorage, convertPrice, convertPriceAll } from '@/utils/Product'
 import { useAppDispatch } from '@/libs/hooks'
 import { addProductCount, deleteAllProduct } from '@/libs/features/productSlice'
+import { Button } from '@mui/material'
+import LocalMallRoundedIcon from '@mui/icons-material/LocalMallRounded';
+import { useRouter } from 'next/navigation'
 
 const Basket = () => {
     const dispatch = useAppDispatch()
+    const router = useRouter()
 
     const [basketProduct, setBasketProduct] = useState<{
         id: number, productId: number, name: string, description: string, image: string, price: number, quantity: number
@@ -131,6 +135,10 @@ const Basket = () => {
         }
     }
 
+    const handleSubmitBasket = () => {
+        router.push('/basket/checkout')
+    }
+
     return (
         <>
             <ToastContainer />
@@ -164,59 +172,72 @@ const Basket = () => {
                     </h6>
                     {
                         basketProduct.length > 0 ?
-                            <div className='w-full flex flex-col gap-y-8 items-center justify-center sm:border border-solid border-brown-100 rounded-lg px-1 py-1 sm:px-2 sm:py-2 md:px-8 md:py-6'>
-                                {
-                                    basketProduct?.map((product) => (
-                                        <div key={product.id} className='w-full flex flex-col'>
-                                            <span className={`w-full h-px rounded-full bg-brown-100/50 mt-1 mb-8 ${product.id == 1 && 'hidden'}`} />
+                            <div className='w-full flex flex-col gap-y-8 items-center justify-center'>
+                                <div className='w-full flex flex-col gap-y-8 items-center justify-center sm:border border-solid border-brown-100 rounded-lg px-1 py-1 sm:px-2 sm:py-2 md:px-8 md:py-6'>
+                                    {
+                                        basketProduct?.map((product) => (
+                                            <div key={product.id} className='w-full flex flex-col'>
+                                                <span className={`w-full h-px rounded-full bg-brown-100/50 mt-1 mb-8 ${product.id == 1 && 'hidden'}`} />
 
-                                            <div className='w-full h-full flex flex-row gap-x-2 sm:gap-x-6 md:gap-x-8 items-stretch rounded-xl px-2 py-4 sm:py-6 md:py-8 justify-between bg-brown-900'>
-                                                <div className='w-2/3 flex flex-col items-end justify-between text-end'>
-                                                    <h3 className='text-white text-sm sm:text-base lg:text-lg line-clamp-2 text-ellipsis'>{product.name}</h3>
-                                                    <h5 className='text-brown-100 text-xs sm:text-sm lg:text-base'>توضیحات: {product.description}</h5>
-                                                    <h6 dir='rtl' className='text-brown-300 text-xs sm:text-xs lg:text-sm'>قیمت: {convertPrice(product.price)} تومان</h6>
-                                                    <h6 dir='rtl' className='text-brown-100 text-xs sm:text-sm lg:text-base'>تعداد: {product.quantity}</h6>
-                                                    <h6 dir='rtl' className='text-brown-300 text-xs sm:text-xs lg:text-sm'>قیمت مجموع: {convertPriceAll(product.price, product.quantity)} تومان</h6>
-                                                </div>
-                                                <div className='w-1/3 flex flex-col items-center justify-center'>
-                                                    <Image
-                                                        className='h-auto w-full hover:brightness-110 transition-all duration-150 cursor-pointer'
-                                                        src={product.image}
-                                                        alt={product.name}
-                                                        width={800}
-                                                        height={800}
-                                                        quality={100}
-                                                        loading='lazy'
-                                                    />
-                                                    <div
-                                                        onClick={(e) => handleProduct(e)}
-                                                        className='flex flex-row items-center justify-between gap-x-2 md:gap-x-4 lg:gap-x-6 border border-solid border-brown-200 rounded-md px-1 py-0.5 sm:px-2 sm:py-1 md:px-4 md:py-2 child:cursor-pointer'>
-                                                        {
-                                                            product.quantity > 1 ?
-                                                                <div className='relative group'>
-                                                                    <RemoveRoundedIcon id={`minus-${product.productId}`} fontSize='small' className='group-hover:bg-brown-700 rounded-full' />
-                                                                    <span id={`minus-${product.productId}`} className='absolute inset-0' />
-                                                                </div> :
-                                                                <div className='relative group'>
-                                                                    <DeleteRoundedIcon fontSize='small' className='group-hover:bg-brown-700 rounded-full' />
-                                                                    <span id={`delete-${product.productId}`} className='absolute inset-0' />
-                                                                </div>
-                                                        }
-                                                        <h5
-                                                            className='text-brown-100 text-sm sm:text-base'
-                                                            style={{ cursor: 'default' }}>
-                                                            {product.quantity}
-                                                        </h5>
-                                                        <div className='relative group'>
-                                                            <AddRoundedIcon fontSize='small' className='group-hover:bg-brown-700 rounded-full' />
-                                                            <span id={`plus-${product.productId}`} className='absolute inset-0' />
+                                                <div className='w-full h-full flex flex-row gap-x-2 sm:gap-x-6 md:gap-x-8 items-stretch rounded-xl px-2 py-4 sm:py-6 md:py-8 justify-between bg-brown-900'>
+                                                    <div className='w-2/3 flex flex-col items-end justify-between text-end'>
+                                                        <h3 className='text-white text-sm sm:text-base lg:text-lg line-clamp-2 text-ellipsis'>{product.name}</h3>
+                                                        <h5 className='text-brown-100 text-xs sm:text-sm lg:text-base'>توضیحات: {product.description}</h5>
+                                                        <h6 dir='rtl' className='text-brown-300 text-xs sm:text-xs lg:text-sm'>قیمت: {convertPrice(product.price)} تومان</h6>
+                                                        <h6 dir='rtl' className='text-brown-100 text-xs sm:text-sm lg:text-base'>تعداد: {product.quantity}</h6>
+                                                        <h6 dir='rtl' className='text-brown-300 text-xs sm:text-xs lg:text-sm'>قیمت مجموع: {convertPriceAll(product.price, product.quantity)} تومان</h6>
+                                                    </div>
+                                                    <div className='w-1/3 flex flex-col items-center justify-center'>
+                                                        <Image
+                                                            className='h-auto w-full hover:brightness-110 transition-all duration-150 cursor-pointer'
+                                                            src={product.image}
+                                                            alt={product.name}
+                                                            width={800}
+                                                            height={800}
+                                                            quality={100}
+                                                            loading='lazy'
+                                                        />
+                                                        <div
+                                                            onClick={(e) => handleProduct(e)}
+                                                            className='flex flex-row items-center justify-between gap-x-2 md:gap-x-4 lg:gap-x-6 border border-solid border-brown-200 rounded-md px-1 py-0.5 sm:px-2 sm:py-1 md:px-4 md:py-2 child:cursor-pointer'>
+                                                            {
+                                                                product.quantity > 1 ?
+                                                                    <div className='relative group'>
+                                                                        <RemoveRoundedIcon id={`minus-${product.productId}`} fontSize='small' className='group-hover:bg-brown-700 rounded-full' />
+                                                                        <span id={`minus-${product.productId}`} className='absolute inset-0' />
+                                                                    </div> :
+                                                                    <div className='relative group'>
+                                                                        <DeleteRoundedIcon fontSize='small' className='group-hover:bg-brown-700 rounded-full' />
+                                                                        <span id={`delete-${product.productId}`} className='absolute inset-0' />
+                                                                    </div>
+                                                            }
+                                                            <h5
+                                                                className='text-brown-100 text-sm sm:text-base'
+                                                                style={{ cursor: 'default' }}>
+                                                                {product.quantity}
+                                                            </h5>
+                                                            <div className='relative group'>
+                                                                <AddRoundedIcon fontSize='small' className='group-hover:bg-brown-700 rounded-full' />
+                                                                <span id={`plus-${product.productId}`} className='absolute inset-0' />
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    ))
-                                }
+                                        ))
+                                    }
+                                </div>
+
+                                <Button
+                                    onClick={handleSubmitBasket}
+                                    className='mt-8 self-start'
+                                    size='large'
+                                    variant="contained"
+                                    color='success'
+                                    startIcon={<LocalMallRoundedIcon />}
+                                >
+                                    تایید و تکمیل سفارش
+                                </Button>
                             </div>
                             :
                             <p className='mt-8 text-brown-100 text-base'>
